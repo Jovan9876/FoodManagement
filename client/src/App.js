5ca383044e8ed40ef1fdd985de3be37ec0693015
RichardOhata <richardohata@gmail.com>
2024-09-26 10:30:12 -0700

    Initialize tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,10 @@ function App() {
         >
           Learn React
         </a>
+
+        <h1 className="text-3xl font-bold underline ">
+      Hello world!
+    </h1>
       </header>
     </div>
   );
