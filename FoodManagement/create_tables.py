8a1eadfc6f58fb2e52d5cf08181bd6516b7955ec
RichardOhata <richardohata@gmail.com>
2024-09-30 15:41:41 -0700

    Implemented http request for food input with hardcoded data

--- FoodManagement/create_tables.py
+++ FoodManagement/create_tables.py
@@ -8,11 +8,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{params["user"]}:{para
 db = SQLAlchemy(app)
 
 # User Table
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    food_items = db.relationship('FoodItem', backref='owner', lazy=True)
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     username = db.Column(db.String(50), unique=True, nullable=False)
+#     password = db.Column(db.String(255), nullable=False)
+#     food_items = db.relationship('FoodItem', backref='owner', lazy=True)
 
 # Food Item Table
 class FoodItem(db.Model):
@@ -22,8 +22,10 @@ class FoodItem(db.Model):
     cost = db.Column(db.Numeric(10, 2), nullable=False)
     expiry_date = db.Column(db.Date, nullable=True)
     nutrition_info = db.Column(db.Text, nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
+# Removed foreign key temporarily
 
 # To create the table
 with app.app_context():
--- FoodManagement/server.py
+++ FoodManagement/server.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request,json, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from models import config
 from models import load_user_foods
+from create_tables import FoodItem
 
 params = config()
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{params["user"]}:{params["password"]}@{params["host"]}:{params["port"]}/{params["database"]}'
 db = SQLAlchemy(app)
 
@@ -15,6 +18,19 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/input_food', methods=['POST'])
+def add_food():
+    data = request.get_json()
+    new_food_item = FoodItem(
+        name=data['name'],
+        quantity=2,
+        cost=2,
+        user_id=4,
+    )
+    db.session.add(new_food_item)
+    db.session.commit()
+    return json.jsonify({"message": "Food item added"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
