c139e046a2996c9b84144455cfe8827c93a24f62
Jovan9876 <jovansandhu3@gmail.com>
2024-10-30 16:38:17 -0700

    Added message for incorrect login or username already taken

--- client/src/pages/LoginInput.js
+++ client/src/pages/LoginInput.js
@@ -1,75 +1,3 @@
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// import { ComplexNavbar } from "../components/Navbar";
-// import { InputDefault } from "../components/InputField";
-// import { SubmitButton } from "../components/Button";
-
-// const Login = () => {
-//   const [userName, setUserName] = useState("");
-//   const [userPassword, setUserPassword] = useState("");
-
-//   async function submitForm() {
-//     const userData = {
-//       username: userName,
-//       password: userPassword,
-//     };
-
-//     try {
-//       const response = await fetch("http://127.0.0.1:5000/login", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         credentials: 'include',
-//         body: JSON.stringify(userData),
-//       });
-
-//       if (response.ok) {
-//         const jsonResponse = await response.json();
-//         console.log(response);
-//         console.log(jsonResponse);
-//         console.log(jsonResponse.message);
-//         window.location.href = "/input";
-//       } else {
-//         console.error("Error:", response.statusText);
-//       }
-//     } catch (error) {
-//       console.error("Fetch error:", error);
-//     }
-//   }
-
-//   return (
-//     <div>
-//       <div className="flex justify-center">
-//         <div className="grid grid-cols-2 gap-36 place-content-between h-48 pt-10">
-//           <InputDefault
-//             label="User Name"
-//             value={userName}
-//             onChange={(e) => setUserName(e.target.value)}
-//           />
-//           <InputDefault
-//             label="Password"
-//             type="password"
-//             value={userPassword}
-//             onChange={(e) => setUserPassword(e.target.value)}
-//           />
-//         </div>
-//       </div>
-
-//       <div className="pl-10 flex justify-center pt-60">
-//         <SubmitButton label="Login" onClick={submitForm} />
-//       </div>
-
-//       <div className="flex justify-center pt-5">
-//         <Link to="/Register">Don't have an account? Register here</Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Login;
-
-
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom"; // Import useNavigate
 import { InputDefault } from "../components/InputField";
@@ -78,6 +6,7 @@ import { SubmitButton } from "../components/Button";
 const Login = ({ onLogin }) => { // Accept onLogin prop
   const [userName, setUserName] = useState("");
   const [userPassword, setUserPassword] = useState("");
+  const [flashMessage, setFlashMessage] = useState('');
   const navigate = useNavigate(); // Initialize useNavigate
 
   async function submitForm() {
@@ -108,7 +37,11 @@ const Login = ({ onLogin }) => { // Accept onLogin prop
         // Use navigate to redirect to the input page
         navigate("/");
       } else {
-        console.error("Error:", response.statusText);
+        if (response.status === 401) {
+          setFlashMessage('Incorrect username or password.');
+        } else {
+          console.error('Error:', response.statusText);
+        }
       }
     } catch (error) {
       console.error("Fetch error:", error);
@@ -118,6 +51,7 @@ const Login = ({ onLogin }) => { // Accept onLogin prop
   return (
     <div className="login-container">
       <h1 className="login-title">Login</h1>
+      {flashMessage && <div className="flash-message">{flashMessage}</div>}
       <div className="login-box">
         <div className="input-grid">
           <InputDefault
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -7,6 +7,7 @@ import {SubmitButton} from '../components/Button'
 const RegisterInput = () => {
   const [userName, setUserName] = useState('');
   const [userPassword, setUserPassword] = useState('');
+  const [flashMessage, setFlashMessage] = useState('');
 
   async function submitForm() {
     const userData = {
@@ -29,31 +30,38 @@ const RegisterInput = () => {
             console.log(jsonResponse.message);
             window.location.href = "/login";
         } else {
+          if (response.status === 409) {
+            setFlashMessage('Username already exists. Please try a different username.');
+          } else {
             console.error('Error:', response.statusText);
+          }
         }
     } catch (error) {
         console.error('Fetch error:', error);
     }
 }
 
-return (
-  <div className="register-container">
-    <h1 className="register-title">Register</h1>
-    <div className="register-box">
-      <div className="input-grid">
-        <InputDefault label="User Name" value={userName} onChange={(e) => setUserName(e.target.value)} />
-        <InputDefault label="Password" type="password" value={userPassword} onChange={(e) => setUserPassword(e.target.value)} />
-      </div>
-      <div className="submit-button">
-        <SubmitButton label="Submit" onClick={submitForm} />
-      </div>
-      <div className="login-link">
-          <Link to="/login">Already have an account? Login here</Link>
-        </div>
-    </div>
+ return (
+   <div className="register-container">
+     <h1 className="register-title">Register</h1>
+      {flashMessage && <div className="flash-message">{flashMessage}</div>}
+     <div className="register-box">
+       <div className="input-grid">
+         <InputDefault label="User Name" value={userName} onChange={(e) => setUserName(e.target.value)} />
+         <InputDefault label="Password" type="password" value={userPassword} onChange={(e) => setUserPassword(e.target.value)} />
+       </div>
+       <div className="submit-button">
+         <SubmitButton label="Submit" onClick={submitForm} />
+       </div>
+       <div className="login-link">
+           <Link to="/login">Already have an account? Login here</Link>
+         </div>
+     </div>
   </div>
 );
 
 };
+
+
 
 export default RegisterInput;
