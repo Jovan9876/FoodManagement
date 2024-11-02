f238eb45a14b02d4c11f3f2f6dbff2f2e2924eff
Richard Ohata <90574753+RichardOhata@users.noreply.github.com>
2024-11-02 14:28:32 -0700

    Removed placeholder navbar elements and added styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -10,7 +10,13 @@ import {
   RocketLaunchIcon,
   Square3Stack3DIcon,
   UserCircleIcon,
-  AcademicCapIcon
+  AcademicCapIcon,
+  PlusCircleIcon,
+  ClipboardDocumentListIcon,
+  CurrencyDollarIcon,
+  BanknotesIcon,
+  CircleStackIcon,
+  BellIcon
 } from '@heroicons/react/24/solid';
 import {
   Avatar,
@@ -25,6 +31,7 @@ import {
   Navbar,
   Typography,
 } from '@material-tailwind/react';
+
 import React from 'react';
 
 // profile menu component
@@ -38,12 +45,8 @@ const profileMenuItems = [
     icon: Cog6ToothIcon,
   },
   {
-    label: 'Inbox',
-    icon: InboxArrowDownIcon,
-  },
-  {
-    label: 'Help',
-    icon: LifebuoyIcon,
+    label: 'Notifications',
+    icon: BellIcon,
   },
   {
     label: 'Sign Out',
@@ -86,7 +89,7 @@ function ProfileMenu() {
             size='sm'
             alt='tania andrew'
             className='border border-gray-900 p-0.5'
-            src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+            src={`${process.env.PUBLIC_URL}/images/profile.png`}
           />
           <ChevronDownIcon
             strokeWidth={2.5}
@@ -119,98 +122,110 @@ function ProfileMenu() {
   );
 }
 
-// nav list menu
-const navListMenuItems = [
-  {
-    title: 'Food Inventory',
-    description: '',
-    link: '/',
-  },
-  {
-    title: 'Expenses',
-    description: '',
-    link: '/expenses',
-  },
-  {
-    title: 'Shopping List',
-    description: '',
-    link: '/shopping',
-  },
-];
+// // nav list menu
+// const navListMenuItems = [
+//   {
+//     title: 'Food Inventory',
+//     description: '',
+//     link: '/',
+//   },
+//   {
+//     title: 'Expenses',
+//     description: '',
+//     link: '/expenses',
+//   },
+//   {
+//     title: 'Shopping List',
+//     description: '',
+//     link: '/shopping',
+//   },
+// ];
 
-function NavListMenu() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+// function NavListMenu() {
+//   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-  const renderItems = navListMenuItems.map(({ title, description, link }) => (
-    <a href={link} key={title}>
-      <MenuItem>
-        <Typography variant='h6' color='blue-gray' className='mb-1'>
-          {title}
-        </Typography>
-        <Typography variant='small' color='gray' className='font-normal'>
-          {description}
-        </Typography>
-      </MenuItem>
-    </a>
-  ));
+//   const renderItems = navListMenuItems.map(({ title, description, link }) => (
+//     <a href={link} key={title}>
+//       <MenuItem>
+//         <Typography variant='h6' color='blue-gray' className='mb-1'>
+//           {title}
+//         </Typography>
+//         <Typography variant='small' color='gray' className='font-normal'>
+//           {description}
+//         </Typography>
+//       </MenuItem>
+//     </a>
+//   ));
 
-  return (
-    <React.Fragment>
-      <Menu allowHover open={isMenuOpen} handler={setIsMenuOpen}>
-        <MenuHandler>
-          <Typography as='a' href='#' variant='small' className='font-normal'>
-            <MenuItem className='hidden items-center gap-2 font-medium text-blue-gray-900 lg:flex lg:rounded-full'>
-              <Square3Stack3DIcon className='h-[18px] w-[18px] text-blue-gray-500' /> Pages{' '}
-              <ChevronDownIcon
-                strokeWidth={2}
-                className={`h-3 w-3 transition-transform ${isMenuOpen ? 'rotate-180' : ''}`}
-              />
-            </MenuItem>
-          </Typography>
-        </MenuHandler>
-        <MenuList className='hidden w-[36rem] grid-cols-7 gap-3 overflow-visible lg:grid'>
-          <Card
-            color='blue'
-            shadow={false}
-            variant='gradient'
-            className='col-span-3 grid h-full w-full place-items-center rounded-md'>
-            <RocketLaunchIcon strokeWidth={1} className='h-28 w-28' />
-          </Card>
-          <ul className='col-span-4 flex w-full flex-col gap-1'>{renderItems}</ul>
-        </MenuList>
-      </Menu>
-      <MenuItem className='flex items-center gap-2 font-medium text-blue-gray-900 lg:hidden'>
-        <Square3Stack3DIcon className='h-[18px] w-[18px] text-blue-gray-500' /> Pages{' '}
-      </MenuItem>
-      <ul className='ml-6 flex w-full flex-col gap-1 lg:hidden'>{renderItems}</ul>
-    </React.Fragment>
-  );
-}
+//   return (
+//     <React.Fragment>
+//       <Menu allowHover open={isMenuOpen} handler={setIsMenuOpen}>
+//         <MenuHandler>
+//           <Typography as='a' href='#' variant='small' className='font-normal'>
+//             <MenuItem className='hidden items-center gap-2 font-medium text-blue-gray-900 lg:flex lg:rounded-full'>
+//               <Square3Stack3DIcon className='h-[18px] w-[18px] text-blue-gray-500' /> Pages{' '}
+//               <ChevronDownIcon
+//                 strokeWidth={2}
+//                 className={`h-3 w-3 transition-transform ${isMenuOpen ? 'rotate-180' : ''}`}
+//               />
+//             </MenuItem>
+//           </Typography>
+//         </MenuHandler>
+//         <MenuList className='hidden w-[36rem] grid-cols-7 gap-3 overflow-visible lg:grid'>
+//           <Card
+//             color='blue'
+//             shadow={false}
+//             variant='gradient'
+//             className='col-span-3 grid h-full w-full place-items-center rounded-md'>
+//             <RocketLaunchIcon strokeWidth={1} className='h-28 w-28' />
+//           </Card>
+//           <ul className='col-span-4 flex w-full flex-col gap-1'>{renderItems}</ul>
+//         </MenuList>
+//       </Menu>
+//       <MenuItem className='flex items-center gap-2 font-medium text-blue-gray-900 lg:hidden'>
+//         <Square3Stack3DIcon className='h-[18px] w-[18px] text-blue-gray-500' /> Pages{' '}
+//       </MenuItem>
+//       <ul className='ml-6 flex w-full flex-col gap-1 lg:hidden'>{renderItems}</ul>
+//     </React.Fragment>
+//   );
+// }
 
 // nav list component
 const navListItems = [
   {
-    label: 'Account',
-    icon: UserCircleIcon,
+    label: 'Food Inventory',
+    icon: CircleStackIcon,
+    color: 'text-blue-600',
+    onClick: () => (window.location.href = 'http://127.0.0.1:3000/'),
   },
   {
-    label: 'Blocks',
-    icon: CubeTransparentIcon,
+    label: 'Expenses',
+    icon: CurrencyDollarIcon,
+    color: 'text-green-500',
+    onClick: () => (window.location.href = 'http://127.0.0.1:3000/expenses'),
   },
   {
-    label: 'Docs',
-    icon: CodeBracketSquareIcon,
+    label: 'Shopping List',
+    icon: ClipboardDocumentListIcon,
+    color: 'text-yellow-600',
+    onClick: () => (window.location.href = 'http://127.0.0.1:3000/shopping'),
+  },
+  {
+    label: 'Add Food',
+    icon: PlusCircleIcon,
+    color: 'text-black',
+    onClick: () => (window.location.href = 'http://127.0.0.1:3000/input'),
   },
 ];
 
 function NavList() {
   return (
     <ul className='mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center'>
-      <NavListMenu />
-      {navListItems.map(({ label, icon }, key) => (
+      {/* <NavListMenu /> */}
+      {navListItems.map(({ label, icon, color, onClick }, key) => (
         <Typography key={label} as='a' href='#' variant='small' color='gray' className='font-medium text-blue-gray-500'>
-          <MenuItem className='flex items-center gap-2 lg:rounded-full'>
-            {React.createElement(icon, { className: 'h-[18px] w-[18px]' })}{' '}
+          <MenuItem className='flex items-center gap-2 lg:rounded-full'onClick={onClick}>
+            {React.createElement(icon, { className: `h-[18px] w-[18px] ${color}`})}{' '}
             <span className='text-gray-900'> {label}</span>
           </MenuItem>
         </Typography>
@@ -234,7 +249,7 @@ export function ComplexNavbar() {
         <Typography
           as='a'
           href='#'
-          className='mr-4 ml-2 cursor-pointer py-1.5 font-medium'
+          className='mr-4 ml-2 cursor-pointer py-1.5 font-semibold text-lg'
           onClick={() => (window.location.href = 'http://127.0.0.1:3000/')}>
           StockedUp
         </Typography>
@@ -249,10 +264,10 @@ export function ComplexNavbar() {
           className='ml-auto mr-2 lg:hidden'>
           <Bars2Icon className='h-6 w-6' />
         </IconButton>
-
-        <Button size='sm' variant='text' onClick={() => (window.location.href = 'http://127.0.0.1:3000/input')}>
+        
+        {/* <Button size='sm' variant='text' onClick={() => (window.location.href = 'http://127.0.0.1:3000/input')}>
           <span>Add Food</span>
-        </Button>
+        </Button> */}
         {/* <Button
           size='sm'
           variant='text'
