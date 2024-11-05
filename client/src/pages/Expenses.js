d17bca1b5dd01492a5bd3a7e7063671aa7daf317
Robert Wilson <robertwilson875@gmail.com>
2024-11-04 22:03:46 -0800

    adjusted expenses and inventory pages, nutrion info works correctly now, etc

--- client/src/pages/Expenses.js
+++ client/src/pages/Expenses.js
@@ -1,31 +1,81 @@
 import * as React from 'react';
 import { useState } from 'react';
+
 export const Expenses = () => {
   const [shoppingList] = useState([
-    { name: 'apples', quantity: 6, price: 3.99, img: 'apples.jpg' },
-    { name: 'bananas', quantity: 4, price: 2.5, img: 'bananas.jpg' },
-    { name: 'bread', quantity: 2, price: 3.0, img: 'bread.jpg' },
-    { name: 'steak', quantity: 5, price: 55.0, img: 'steak.jpg' },
+    {
+      id: 1,
+      name: 'apples',
+      cost: 3.99,
+      quantity: 6,
+      nutrition_info: { calories: 52, fat: 0.2, carbs: 14, protein: 0.3 },
+    },
+    {
+      id: 2,
+      name: 'bananas',
+      cost: 2.5,
+      quantity: 4,
+      nutrition_info: { calories: 89, fat: 0.3, carbs: 23, protein: 1.1 },
+    },
+    {
+      id: 3,
+      name: 'bread',
+      cost: 3.0,
+      quantity: 2,
+      nutrition_info: { calories: 265, fat: 3.2, carbs: 49, protein: 9 },
+    },
+    {
+      id: 4,
+      name: 'steak',
+      cost: 55.0,
+      quantity: 5,
+      nutrition_info: { calories: 679, fat: 48, carbs: 0, protein: 62 },
+    },
   ]);
+
   const [totalExpenses] = useState(() => {
-    return shoppingList.reduce((acc, item) => acc + item.price, 0);
+    return shoppingList.reduce((acc, item) => acc + item.cost, 0);
   });
+
+  const normalizeItemName = (name) => {
+    const normalizedNames = {
+      bread: 'bread',
+    };
+
+    // Check if the name contains any of the keys in normalizedNames
+    for (const key in normalizedNames) {
+      if (name.toLowerCase().includes(key)) {
+        return normalizedNames[key];
+      }
+    }
+
+    // If the name is not in the normalizedNames, add it and return the plural form by default
+    const normalizedName = name.toLowerCase();
+    const pluralName = normalizedName.endsWith('s') ? normalizedName : `${normalizedName}s`;
+    normalizedNames[normalizedName] = pluralName;
+    return pluralName;
+  };
+
   return (
-    <div className=' justify-items-center'>
+    <div className='justify-items-center'>
       <div>
         <header style={{ fontSize: '40px', justifyItems: 'center', paddingTop: '20px' }}>
           <h4>
             <b>Expenses Sheet:</b>
           </h4>
         </header>
-        <ul className='w-96 outline outline-offset-2 outline-6 outline-light-green-800 pb-3 sm:pb-4 pt-1 pl-5 pr-2 mt-10'>
+        <ul className='w-96 outline outline-offset-2 outline-6 outline-light-green-800 pb-3 sm:pb-4 pt-1 pl-5 pr-2 mt-4'>
           {shoppingList.map((item, index) => (
             <li
               key={index}
               className='w-full border-b-4 border-neutral-100 border-opacity-100 py-4 dark:border-opacity-50'>
-              <div className='flex items-center space-x-12 rtl:space-x-revers '>
-                <img class='w-8 h-8 rounded-full mr-6' src={`/${item.name}.jpg`} alt='Bananas' />
-                {item.name}: Quantity: {item.quantity} Price: ${item.price}
+              <div className='flex items-center space-x-12 rtl:space-x-reverse'>
+                <img
+                  className='w-16 h-16 rounded-full mr-6'
+                  src={`/${normalizeItemName(item.name)}.jpg`}
+                  alt={item.name}
+                />
+                {item.name}: Quantity: {item.quantity} Price: ${item.cost}
               </div>
             </li>
           ))}
@@ -33,15 +83,12 @@ export const Expenses = () => {
       </div>
 
       <div>
-        <header style={{ fontSize: '40px', justifyItems: 'center', paddingTop: '50px' }}>
+        <header style={{ fontSize: '40px', justifyItems: 'center', paddingTop: '20px' }}>
           <h4>
             <b>Expenses Summary:</b>
           </h4>
         </header>
-        <div className='flex items-center space-x-12 rtl:space-x-revers w-96 outline outline-offset-2 outline-6 outline-light-green-800 pb-3 sm:pb-4 pt-1 pl-5 pr-2 mt-10'>
-          <p>Total Expenses: </p>
-          <p>{`\$${totalExpenses}`}</p>
-        </div>
+        <p>Total Expenses: ${totalExpenses}</p>
       </div>
     </div>
   );
--- client/src/pages/Inventory.js
+++ client/src/pages/Inventory.js
@@ -1,10 +1,15 @@
 import { Button, Dialog, DialogBody, DialogFooter, DialogHeader } from '@material-tailwind/react';
 import * as React from 'react';
+
 export const Inventory = () => {
   const [inventory, setInventory] = React.useState([]);
-
   const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(!open);
+  const [selectedItem, setSelectedItem] = React.useState(null);
+
+  const handleOpen = (item) => {
+    setSelectedItem(item);
+    setOpen(!open);
+  };
 
   React.useEffect(() => {
     async function fetchData() {
@@ -38,65 +43,88 @@ export const Inventory = () => {
     }
     void fetchData();
   }, []);
-  console.log('inventory is :::::::::<br></br>');
-  console.log(inventory);
+
+  const normalizeItemName = (name) => {
+    const normalizedNames = {
+      apple: 'apples',
+      carrot: 'carrots',
+      pizza: 'pizzas',
+      steak: 'steaks',
+      sushi: 'sushi',
+      bread: 'bread',
+      // Add more mappings as needed
+    };
+
+    for (const key in normalizedNames) {
+      if (name.toLowerCase().includes(key)) {
+        return normalizedNames[key];
+      }
+    }
+
+    const normalizedName = name.toLowerCase();
+    return normalizedName.endsWith('s') ? normalizedName : `${normalizedName}s`;
+  };
 
   return (
     <div className='justify-items-center pt-8'>
-      <ul class='flex flex-col space-y-8 max-w-md divide-y pl-8'>
+      <ul className='flex flex-col space-y-4 max-w-xl divide-y pl-8'>
         {inventory.map((item) => (
           <li
-            class='outline outline-offset-2 outline-6 outline-light-green-800 rounded-3xl pb-3 sm:pb-4 pt-4 pl-5 pr-2'
+            className='outline outline-offset-2 outline-6 outline-light-green-800 rounded-3xl pb-3 sm:pb-4 pt-4 pl-5 pr-2'
             key={item.id}>
-            <div class='flex items-center space-x-2 rtl:space-x-reverse'>
-              <div class='flex-shrink-0'>
-                <img class='w-8 h-8 rounded-full' src={`/${item.name}.jpg`} alt='Bananas' />
+            <div className='flex items-center space-x-4 rtl:space-x-reverse'>
+              <div className='flex-shrink-0 flex justify-center items-center w-16 h-16'>
+                <img className='w-16 h-16 rounded-full' src={`/${normalizeItemName(item.name)}.jpg`} alt={item.name} />
               </div>
-              <div class='flex-1 min-w-0 pl-2'>
-                <p class='text-sm font-medium text-gray-900 truncate dark:text-white'>{item.name}</p>
-              </div>
-              <div class='inline-flex items-center text-base font-semibold text-gray-900 dark:text-white pr-6'>
-                <p class='text-sm font-medium text-gray-900 truncate dark:text-white'>Stock:</p>
-                {item.quantity}
-                <Button onClick={handleOpen} variant='text' color='green' size='sm'>
+              <div className='flex-1 min-w-0 pl-2 flex flex-col items-center'>
+                <p className='text-sm font-medium text-gray-900 truncate dark:text-white'>{item.name}</p>
+                <Button onClick={() => handleOpen(item)} variant='text' color='green' size='sm'>
                   NUTRITION
                 </Button>
-                <Dialog open={open} handler={handleOpen} size='xs'>
-                  <DialogHeader>NUTRITIONAL INFO:</DialogHeader>
-                  <DialogBody>
-                    <p class='text-sm font-medium text-gray-900 truncate dark:text-white'>
-                      CALORIES: {`\t${item.nutrition_info.calories}`}
-                      <br></br>
-                      FAT: {`\t${item.nutrition_info.fat}`}
-                      <br></br>
-                      CARBS: {`\t${item.nutrition_info.carbs}`}
-                      <br></br>
-                      PROTEIN: {`\t${item.nutrition_info.protein}`}
-                    </p>
-                  </DialogBody>
-                  <DialogFooter>
-                    <Button variant='text' color='green' onClick={handleOpen} className='mr-1'>
-                      <span>CLOSE</span>
-                    </Button>
-                  </DialogFooter>
-                </Dialog>
               </div>
-              <div class='inline-flex items-center text-base font-semibold text-gray-900 dark:text-white'>
-                <p class='text-sm font-medium text-gray-900 truncate dark:text-white'>Price:</p>
-                {item.cost}
+              <div className='flex flex-col items-start text-base font-semibold text-gray-900 dark:text-white'>
+                <div className='inline-flex items-center'>
+                  <p className='text-sm font-medium text-gray-900 truncate dark:text-white'>Price:</p>
+                  {item.cost}
+                </div>
+                <div className='inline-flex items-center'>
+                  <p className='text-sm font-medium text-gray-900 truncate dark:text-white'>Stock:</p>
+                  {item.quantity}
+                </div>
               </div>
-              <Button 
-                onClick={() => window.location.href = `http://127.0.0.1:3000/input/${item.name}`} // Navigate to the input page for this food item
+              <Button
+                onClick={() => (window.location.href = `http://127.0.0.1:3000/input/${item.name}`)} // Navigate to the input page for this food item
                 variant='outlined'
                 color='blue'
-                size='sm'
-              >
+                size='sm'>
                 Update
               </Button>
             </div>
           </li>
         ))}
       </ul>
+
+      {selectedItem && (
+        <Dialog open={open} handler={() => setOpen(!open)} size='xs'>
+          <DialogHeader>NUTRITIONAL INFO FOR: {selectedItem.name.toUpperCase()}</DialogHeader>
+          <DialogBody>
+            <p className='text-sm font-medium text-gray-900 truncate dark:text-white'>
+              CALORIES: {`\t${selectedItem.nutrition_info.calories}`}
+              <br></br>
+              FAT: {`\t${selectedItem.nutrition_info.fat}`}
+              <br></br>
+              CARBS: {`\t${selectedItem.nutrition_info.carbs}`}
+              <br></br>
+              PROTEIN: {`\t${selectedItem.nutrition_info.protein}`}
+            </p>
+          </DialogBody>
+          <DialogFooter>
+            <Button variant='text' color='green' onClick={() => setOpen(!open)} className='mr-1'>
+              <span>CLOSE</span>
+            </Button>
+          </DialogFooter>
+        </Dialog>
+      )}
     </div>
   );
 };
