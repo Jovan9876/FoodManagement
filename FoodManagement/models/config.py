464bc781a1b4827b6d936e0b227ab74823566a92
Jovan9876 <jovansandhu3@gmail.com>
2024-11-22 19:29:16 -0800

    test

--- FoodManagement/models/config.py
+++ FoodManagement/models/config.py
@@ -25,12 +25,6 @@ def config(filename="database.ini", section="mysql"):
     db = {}
 
     # First, check for environment variables (for CI/CD environments like GitHub Actions)
-    # db["user"] = os.getenv("MYSQL_USER", "test")
-    # db["password"] = os.getenv("MYSQL_PASSWORD", "test")
-    # db["host"] = os.getenv("MYSQL_HOST", "mysql")
-    # db["database"] = os.getenv("MYSQL_DB", "FoodManagement")
-    # db["port"] = os.getenv("MYSQL_PORT", "3306")
-    # db["redis"] = os.getenv("REDIS", "redis")
 
     # If not found, fallback to reading from the configuration file
     if not all(db.values()):
@@ -41,6 +35,13 @@ def config(filename="database.ini", section="mysql"):
             for param in params:
                 db[param[0]] = param[1]
         else:
-            raise Exception(f"Section {section} not found in the {filename} file")
+            # github actions
+            db["user"] = os.getenv("MYSQL_USER", "test")
+            db["password"] = os.getenv("MYSQL_PASSWORD", "test")
+            db["host"] = os.getenv("MYSQL_HOST", "127.0.0.1")
+            db["database"] = os.getenv("MYSQL_DB", "FoodManagement")
+            db["port"] = os.getenv("MYSQL_PORT", "3306")
+            db["redis"] = os.getenv("REDIS", "127.0.0.1")
+            # raise Exception(f"Section {section} not found in the {filename} file")
 
     return db
